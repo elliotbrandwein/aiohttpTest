3a81da118639a01781ed972ae9d6e6c908593101
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-28 12:50:49 -0400

    now sending json in the ajax

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-import asyncio
 import aiohttp
+import asyncio
 import aiohttp_jinja2
 import jinja2
 from aiohttp import web
@@ -23,15 +23,17 @@ async def handler(request):
     response.headers['Content-Language'] = 'eng'
     return response
 
-async def handlePost(request):
-    context = request.data
-    print(context)
+async def handle_post(request):
+    x = await request.json()
+    print(x)
+    context=x
     response = aiohttp_jinja2.render_template('simple.jinja2', request, context)
     response.headers['Content-Language'] = 'eng'
     return response
+
 app = web.Application()
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('/home/elliot/PycharmProjects/aiohttpTest/templates'))
 app.router.add_route('GET', '/', handler)
-app.router.add_route('POST', '/', handlePost)
+app.router.add_route('POST', '/', handle_post)
 
 web.run_app(app)
